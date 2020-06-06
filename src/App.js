a0144667265977d9c49ed1ba4ac82ce82f04e8b7
eshwarjoshiPsl <eshwar_joshi@persistent.com>
2020-06-06 18:14:05 +0530

    commit for react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RoutingPage from './Common/RoutingPage';
+
+function App() {
+  return (
+    <div>
+      <RoutingPage />
+    </div>
+  );
+}
+
+export default App;
